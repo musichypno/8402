05daa803b587bd112439da980b72fd205aee013d
musichypno <46890147+musichypno@users.noreply.github.com>
2023-11-13 14:37:34 -0600

    Update tile.js

--- js/tile.js
+++ js/tile.js
@@ -3,17 +3,17 @@ function Tile(position, value) {
   this.y                = position.y;
   this.value            = value || 2;
   this.flashingText     = [
-    [2, ["This","IS","TWO"],
-    [4, ["This","IS","FOUR"],
-    [8, ["This","IS","EIGHT"],
-    [16, ["This","IS","16"],
-    [32, ["This","IS","32"],
-    [64, ["This","IS","64"],
-    [128, ["This","IS","128"],
-    [256, ["This","IS","256"],
-    [512, ["This","IS","512"],
-    [1024, ["This","IS","1024"],
-    [2048, ["This","IS","2048"]
+    [2, ["This","IS","TWO"]],
+    [4, ["This","IS","FOUR"]],
+    [8, ["This","IS","EIGHT"]],
+    [16, ["This","IS","16"]],
+    [32, ["This","IS","32"]],
+    [64, ["This","IS","64"]],
+    [128, ["This","IS","128"]],
+    [256, ["This","IS","256"]],
+    [512, ["This","IS","512"]],
+    [1024, ["This","IS","1024"]],
+    [2048, ["This","IS","2048"]]
 ];
   this.flashingWord = null;
   this.previousPosition = null;
